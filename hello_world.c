dc983afe4047c0cf8888a372db4ef987c58df543
lebavui <lebavui@hotmail.com>
2023-03-21 17:04:41 +0700

    Them chuong trinh Hello World

---
+++ hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    int sum = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+        sum += i;
+        printf("sum = %d\n", sum);
+    }
+        
+    return 0;
+}
