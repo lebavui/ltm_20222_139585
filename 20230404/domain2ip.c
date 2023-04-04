2e37a0bc8493cc7ee9b14849e644ee4fdebc5163
lebavui <lebavui@hotmail.com>
2023-04-04 15:53:24 +0700

    Thêm ví dụ phân giải tên miền

---
+++ 20230404/domain2ip.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    struct addrinfo *result, *p;
+    int ret = getaddrinfo("hust.edu.vn", "http", NULL, &result);
+
+    if (ret != 0)
+    {
+        printf("Failed to get IP: %s\n", gai_strerror(ret));
+        return 1;
+    }
+    
+    printf("Success to get IP.\n");
+
+    p = result;
+    while (p != NULL)
+    {
+        if (p->ai_family == AF_INET)
+        {
+            printf("IPv4: %s\n", 
+                inet_ntoa(((struct sockaddr_in *)p->ai_addr)->sin_addr));
+            
+        } 
+        else if (p->ai_family == AF_INET6)
+        {
+            char buf[64];
+            printf("IPv6: %s\n", inet_ntop(AF_INET6, 
+                &((struct sockaddr_in6 *)p->ai_addr)->sin6_addr,
+                buf, INET6_ADDRSTRLEN));
+        }
+        p = p->ai_next;
+    }
+    
+
+    return 0;
+}
